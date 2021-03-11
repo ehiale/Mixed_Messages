bb2860f8421cc822db68069a75b5aeb7c653fda7
ehiale <ma.venturoli5@gmail.com>
2021-03-11 17:32:07 +0000

    First Committ
    Add first script.js and .txt files

---
+++ script.js.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const animalsTextFile = fs.readFileSync("animals.txt", "utf8");
+const animal = animalsTextFile.split("\n");
+const verbsTextFile = fs.readFileSync("regular verbs.txt", "utf8");
+const verb = verbsTextFile.split("\n");
+const adjectivesTextFile = fs.readFileSync("adjectives.txt", "utf8");
+const adjective = adjectivesTextFile.split("\n");
+const nounsTextFile = fs.readFileSync("nouns.txt", "utf8");
+const noun = nounsTextFile.split("\n");
+const conjunctions = ['and', 'but', 'or', 'yet'];
+const randomItem = (arr) => {
+    let item = arr[Math.floor(Math.random()*arr.length)];
+    return item;
+}
+const randomSentence = ['The', randomItem(animal), randomItem(animal), randomItem(verb) + 's', 'the', 
+randomItem(adjective), randomItem(noun), randomItem(conjunctions), 'it', randomItem(verb) + 's', 'the', randomItem(adjective), 
+randomItem(noun)]
+console.log(randomSentence.join(' ') + '.');
